8ad2c2d8f18b2672f7c76852b2b5d14c9b6178ce
juankafdez05 <juanka.fdez05@gmail.com>
2025-03-19 19:31:39 +0100

    FuncionesJSON.py

--- FuncionesJSON.py
+++ FuncionesJSON.py
@@ -1,4 +1,7 @@
+
 import json
+
+
 
 def GanadoresBalonDeOro(doc):
     ano_gan = int(input("Introduzca el año :" ))
@@ -10,8 +13,19 @@ def GanadoresBalonDeOro(doc):
     
     return gana
     
+def MedioCentrosGanadores(doc):
+    medio= []
+    posiciones_mediocentros = {"Mediocentro", "Centrocampista" , "Mediapunta" , "Mediocampista"}
+    nombres_vistos = set()
 
- 
+    for ganadores in doc["balon_de_oro"]["ganadores"]:
+        if ganadores["jugador"]["posicion"] in posiciones_mediocentros and ganadores["jugador"]["nombre"] not in nombres_vistos:
+            
+            medio.append(ganadores["jugador"]["nombre"])
+            nombres_vistos.add(ganadores["jugador"]["nombre"])
+
+    return medio
+    
 
 with open("JSONproyecto") as fichero:
     doc=json.load(fichero)
@@ -23,3 +37,10 @@ print("¿Que año quieres consultar?")
 for gana in GanadoresBalonDeOro(doc):
     if gana != 0 :
         print("El ganador el fue: ", gana)
+
+#Ejercicio 2
+
+for medio in MedioCentrosGanadores(doc):
+    print("El Balon de Oro siendo mediocentro lo han ganado", len(MedioCentrosGanadores(doc)), "Estos son: ", medio )
+    print("Nombres:", ", ".join(MedioCentrosGanadores(doc)))
+    
